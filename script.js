35055a560bd6920dcff5c8d712648499c383dd04
Artem382-e <76814276+Artem382-e@users.noreply.github.com>
2021-10-15 17:22:50 +0400

    index.html; script.js
    I added two files.
    The first file is the main one - index.html
    The second JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+'uze strict';
+
+let userName = prompt("Введите имя", "Алиса");
+let isTeaWanted = confirm("Вы хотите чаю?");
+
+alert( "Посетитель: " + userName ); // Алиса
+alert( "Чай: " + isTeaWanted ); // true
